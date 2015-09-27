3e2ad76276359e3a009ee9c0643df13a8527ae15
Qing Yi Li <qinggyili@gmail.com>
2015-09-26 23:53:20 -0700

    Added k-fold CV.R

---
+++ Kfold CV.R
@@ -0,0 +1,30 @@
+#======K-FOLD CV function====
+CV<-function(k,f,dat,y){
+  
+  size=dim(dat)[1]
+  ss=size/k
+  s=sample(1:size,size) #shuffling
+  index=1
+  #extract names from fit 
+  var= names(f$coefficients[-1])
+  v= paste(var, collapse="+")
+  s1<- paste(deparse(substitute(y)),"~")
+  s2<-paste("te$",deparse(substitute(y)),sep="")
+  mspe<-(1:k)
+  formula=paste(s1,v)
+  for(i in 1:k){
+    end=index+ss-1
+    spl=s[index:end]
+    te<-dat[spl,]
+    tr<-dat[-spl,]
+    ff=lm(as.formula(formula), data =tr)
+    fx = predict(ff, new = te)
+    te.y<-(eval(parse(text=s2)))
+    m=(mean(te.y-fx))
+       
+    mspe[i]=(m)
+    index=i*ss+1
+  }
+  print((mean(mspe)))
+}
+
